60466b2846aed47a90fc23b168523dea3d44ea75
Charles F. Munat <charles@paperhat.ltd>
2018-01-24 14:27:27 +1300

    First pass

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # gatsby-remark-line-numbering
-Add line numbering to syntax-highlighted blocks
+
+Add line numbering to markdown code blocks.
+
+## Install
+
+`yarn add gatsby-transformer-remark gatsby-remark-line-numbering`
+
+or
+
+`npm install -S gatsby-transformer-remark gatsby-remark-line-numbering`
+
+## How to use
+
+```javascript
+// In your gatsby-config.js file:
+plugins: [
+  resolve: `gatsby-transformer-remark`,
+  options {
+    plugins: [
+      `gatsby-remark-line-numbering`
+    ]
+  }
+]
---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const visit = require('unist-util-visit')
+const R = require('ramda')
+
+/* Line numbering */
+const addLineNumberSpans = R.map(
+  code => `<span class="line-numbered">${code}</span>`
+)
+
+const swapTest = /<span class="line-numbered"><\/span>/g
+const swapReplace = '</span><span class="line-numbered>'
+const swapSpanClosingTags = R.map(R.replace(swapTest, swapReplace))
+
+const orphanTest = /^<\/span><span class="line-numbered">$/
+const orphanReplace = ''
+const replaceOrphans = R.map(R.replace(orphanTest, orphanReplace))
+
+const combineTest = /<span class="line-numbered"><span class="gatsby-highlight-code-line">(.*)<\/span>$/g
+const combineReplace = '<span class="gatsby-highlight-code-line line-numbered">$1'
+const combineSpans = R.map(R.replace(combineTest, combineReplace))
+
+const addLineNumbers = R.pipe(
+  R.split('\n'),
+  addLineNumberSpans,
+  swapSpanClosingTags,
+  replaceOrphans,
+  combineSpans,
+  R.join('\n')
+)
+
+module.exports = function ({ markdownAST }) {
+  visit(markdownAST, `html`, function (node) {
+    const [, pre, inner, post] = R.match(
+      /(.+)<code>(.+)<\/code>(.+)/s,
+      node.value
+    )
+
+    if (inner) {
+      const updated = R.trim(addLineNumbers(inner))
+
+      node.value = `${pre}${updated}${post}`
+    }
+  })
+
+  return markdownAST
+}
